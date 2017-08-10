e328c07c33b9c06b002708fd22845ff3f3aa5e38
Abid Nurul Hakim <abidnurulhakim@gmail.com>
2017-08-10 23:57:34 +0700

    fix bug model

--- src/Diff/Diff.php
+++ src/Diff/Diff.php
@@ -43,7 +43,7 @@ class Diff
      */
     protected function __construct($activity)
     {
-        $this->model = new $activity;
+        $this->model = new $activity->model;
 
         $this->before = $this->data($activity->before);
 
